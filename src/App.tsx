81444ca7d76d3aa9d5780f01f6099b3d70696b81
fito-cheol <fito-cheol@example.com>
2025-10-22 17:39:18 +0900

    Make Examples the main page: redirect / to /examples, remove Home and navbar link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
 import Examples from "./pages/Examples";
 import ExamplesLayout from "./layouts/ExamplesLayout";
+import { Navigate } from "react-router-dom";
 import BasicForm from "./pages/examples/BasicForm";
 import Validation from "./pages/examples/Validation";
 import DynamicFields from "./pages/examples/DynamicFields";
@@ -22,9 +22,6 @@ function App() {
           </div>
           <ul className="nav-links">
             <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
               <Link to="/examples">Examples</Link>
             </li>
           </ul>
@@ -32,7 +29,7 @@ function App() {
 
         <main className="main-content">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Navigate to="/examples" replace />} />
             <Route path="/examples" element={<ExamplesLayout />}>
               <Route index element={<Examples />} />
               <Route path="basic-form" element={<BasicForm />} />
